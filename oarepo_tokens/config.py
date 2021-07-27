ef1695a730b15da070270683be3ff816acf4c512
Tom Hlava <email = hlava@cesnet.cz>
2021-07-27 15:05:02 +0200

    added deploy CI step

--- oarepo_tokens/config.py
+++ oarepo_tokens/config.py
@@ -5,6 +5,4 @@
 # OARepo-Tokens is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
-OAREPO_TOKENS_TOKEN_NAME = '_oarepo_upload_token'
-# OAREPO_TOKENS_TOKEN_TTL = 3600
 OAREPO_TOKENS_TOKEN_TTL = 10800
